26c89e130c1a9abe07ea684a369116f77407e46c
saggydhage <59614207+saggydhage@users.noreply.github.com>
2020-11-12 14:41:48 +0530

    firstCommit

---
+++ src/main/java/studi/co/pageModules/Module_Login.java
@@ -0,0 +1,32 @@
+package studi.co.pageModules;
+
+import studi.co.Base.*;
+import studi.co.pageObjects.*;
+
+public class Module_Login extends BaseClass {
+
+	public static String homepage_title;
+	
+	
+	public void Login_to_app() throws InterruptedException {
+
+		Object_Login login = new Object_Login();
+
+		login.username.clear();
+		login.username.sendKeys(prop.getProperty("username_password"));
+		login.password.clear();
+		login.password.sendKeys(prop.getProperty("username_password"));
+		login.login_button.click();
+		
+		Thread.sleep(5000);
+		
+		homepage_title = login.homepage_title.getText();
+		
+		if(homepage_title.equals("What would\n" + "you like to do next?"))
+			System.out.println("Login successfull");
+		else 
+			System.out.println("Login Failed");
+
+	}
+
+}
